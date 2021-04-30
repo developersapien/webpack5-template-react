b3e7afae479dc5cb774e36c68819b5195ee05cbf
Hakan Hueriyet <hakan.hueriyet@justeattakeaway.com>
2021-04-30 15:03:01 +0200

    redundant files removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import './styles.css'
 const App = () => {
-  const name = 'sasasa'
   return (
     <div>
       <h1>
-        Helloriko * {process.env.NODE_ENV} {process.env.name}
+        Welcome * {process.env.NODE_ENV} {process.env.name}
       </h1>
     </div>
   )
